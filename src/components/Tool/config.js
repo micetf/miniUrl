0cad6650cbebfca61317934dbe61cc3f29a08def
MiCetF <webmaster@micetf.fr>
2022-02-25 21:57:59 +0100

    First commit

---
+++ src/components/Tool/config.js
@@ -0,0 +1,2 @@
+export const YOURLS_API = "https://micetf.fr/urls/yourls-api.php";
+export const YOURLS_SIGNATURE = "e98a5768b5";
---
+++ src/components/Tool/index.js
@@ -0,0 +1,130 @@
+import axios from "axios";
+import { useRef, useState } from "react";
+import { YOURLS_API, YOURLS_SIGNATURE } from "./config.js";
+import modal from "bootstrap/js/dist/modal";
+
+function verifierURL(url) {
+    const res = url.match(
+        /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_.~#?&//=]*)/g
+    );
+    return res !== null;
+}
+
+function Tool() {
+    const [lien, setLien] = useState("");
+    const [texte, setTexte] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [lienRaccourci, setLienRaccourci] = useState("");
+    const modalRef = useRef(null);
+
+    function handleButtonClick(e) {
+        e.preventDefault();
+        if (lien !== "" && verifierURL(lien) !== null) {
+            setLoading(true);
+            axios
+                .get(YOURLS_API, {
+                    params: {
+                        signature: YOURLS_SIGNATURE,
+                        action: "shorturl",
+                        format: "json",
+                        url: lien,
+                        keyword: texte,
+                    },
+                })
+                .then(function (response) {
+                    setLienRaccourci(
+                        "https://micetf.fr/urls/" + response.data.url.keyword
+                    );
+                    setLoading(false);
+                    new modal(modalRef.current).show();
+                })
+                .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                });
+        }
+    }
+    function handleInputChange(e) {
+        e.preventDefault();
+        const value = e.target.value.trim();
+        switch (e.target.name) {
+            case "lien":
+                setLien(value);
+                break;
+            case "texte":
+                setTexte(value);
+                break;
+        }
+    }
+    function closeModal() {
+        setLienRaccourci("");
+        new modal(modalRef.current).hide();
+    }
+
+    return (
+        <>
+            <div className="container">
+                <div className="d-flex flex-column justify-content-around vh-restant">
+                    <div className="text-center">
+                        <div className="h1">Créer un lien raccourci</div>
+                        <div className="h2 text-muted fst-italic">
+                            pour partager simplement une ressource en ligne
+                        </div>
+                    </div>
+                    <div className="input-group">
+                        <input
+                            type="text"
+                            className="form-control  form-control-lg"
+                            placeholder="Lien à raccourcir"
+                            value={lien}
+                            name="lien"
+                            onChange={handleInputChange}
+                        />
+                    </div>
+                    <div className="input-group">
+                        <span className="input-group-text">
+                            https://micetf.fr/urls/
+                        </span>
+                        <input
+                            type="text"
+                            className="form-control  form-control-lg"
+                            placeholder="Texte personnalisé (facultaitf)"
+                            value={texte}
+                            name="texte"
+                            onChange={handleInputChange}
+                        />
+                    </div>
+                    <div className="container">
+                        <button
+                            className="btn btn-primary btn-lg"
+                            onClick={handleButtonClick}
+                        >
+                            Raccourcir
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div ref={modalRef} className="modal fade" tabIndex="-1">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h3 className="modal-title">Lien raccourci</h3>
+                            <button
+                                type="button"
+                                className="btn-close"
+                                data-bs-dismiss="modal"
+                                aria-label="Close"
+                                onClick={closeModal}
+                            ></button>
+                        </div>
+                        <div className="modal-body">
+                            <p>{lienRaccourci}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Tool;
